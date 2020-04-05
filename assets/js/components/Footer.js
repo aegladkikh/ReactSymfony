b0d4edd00de83ad08f7bd6219c4a457c839b4ee7
Slip <tolikslip@mail.ru>
2020-04-05 18:22:01 +0700

    Add 0.6: footer

--- assets/js/components/Footer.js
+++ assets/js/components/Footer.js
@@ -3,10 +3,22 @@ import React, {Component} from 'react';
 export class Footer extends Component {
 
     render() {
+        const Year = new Date().getFullYear();
+
         return (
             <footer className="footer py-3 fixed-bottom bg-dark">
                 <div className="container">
-                    <span className="text-muted">Тестовое приложение.</span>
+                    <div className="row">
+                        <div className="col-6">
+                            <p>© {Year} a.gladkikh</p>
+                            <p>
+                                <span className="text-muted">Тестовое приложение.</span>
+                            </p>
+                        </div>
+                        <div className="col-6">
+                            <p className="float-right"><a href="#">Наверх</a></p>
+                        </div>
+                    </div>
                 </div>
             </footer>
         )
